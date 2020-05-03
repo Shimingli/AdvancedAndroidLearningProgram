833d50f7a3f14a79fc9a8c7a04c09376d65e098a
shiming.li <lamshiming@sina.com>
2020-05-03 21:10:47 +0800

    init aidl

---
+++ app/src/main/java/com/lsm/advancedandroid/learningprogram/aidl_demo/aidl/Registry.java
@@ -0,0 +1,37 @@
+package com.lsm.advancedandroid.learningprogram.aidl_demo.aidl;
+
+/**
+ * @NAME: Registry
+ * @Des:
+ * @USER: shiming
+ * @DATE: 2020/5/3 15:24
+ * @PROJECT_NAME: AdvancedAndroidLearningProgram
+ */
+public class Registry {
+
+    private static Registry instance;
+
+    private Registry(){
+
+    }
+
+    public static Registry getInstance() {
+        if (instance==null){
+            synchronized (Registry.class){
+                if (instance==null){
+                    instance =new Registry();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 做两张表 服务表class 标记
+     * @param clazz
+     */
+    public void register(Class<?> clazz) {
+
+        clazz.getAnnotation()
+    }
+}
