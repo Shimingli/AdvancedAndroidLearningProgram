be7c75f7b7b6660c0cd9e89325c4de6ccb83b518
shiming.li <lamshiming@sina.com>
2020-05-03 21:11:58 +0800

    init aidl

--- app/src/main/java/com/lsm/advancedandroid/learningprogram/aidl_demo/aidl/Registry.java
+++ app/src/main/java/com/lsm/advancedandroid/learningprogram/aidl_demo/aidl/Registry.java
@@ -2,7 +2,7 @@ package com.lsm.advancedandroid.learningprogram.aidl_demo.aidl;
 
 /**
  * @NAME: Registry
- * @Des:
+ * @Des:  IPCFrame
  * @USER: shiming
  * @DATE: 2020/5/3 15:24
  * @PROJECT_NAME: AdvancedAndroidLearningProgram
